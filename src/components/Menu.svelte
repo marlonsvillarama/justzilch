9714538ed667e1076408acc7295d554cd1002300
Marlon Villarama <marlonsvillarama@gmail.com>
2025-06-17 22:29:19 +1200

    20250617

---
+++ src/components/Menu.svelte
@@ -0,0 +1,66 @@
+<script>
+
+</script>
+
+<nav>
+    <ul>
+        <li>
+            <img src="/images/chart.svg">
+            <a href="/">Dashboard</a>
+        </li>
+        <li>
+            <img src="/images/donor.svg">
+            <a href="#">Donors</a>
+        </li>
+        <li>
+            <img src="/images/community.svg">
+            <a href="#">Community Groups</a>
+        </li>
+        <li>
+            <img src="/images/volunteer.svg">
+            <a href="#">Volunteers</a>
+        </li>
+        <li>
+            <img src="/images/data.svg">
+            <a href="#">Data Import</a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        /* border: 1px solid green; */
+        font-size: var(--fs-xs);
+        min-width: calc(var(--spacing) * 64);
+        /* padding: 1rem; */
+    }
+    ul {
+        list-style: none;
+    }
+    li {
+        align-items: center;
+        /* border: 1px solid red; */
+        cursor: pointer;
+        display: flex;
+        flex-direction: row;
+        gap: 1.5rem;
+        padding: 0.75rem 1.5rem;
+        transition: 150ms all ease-in-out;
+    }
+    li:hover {
+        background-color: var(--color-border);
+    }
+    li > img {
+        color: var(--color-font-lite);
+        width: 1.5rem;
+    }
+    li > a {
+        color: var(--color-font-lite);
+        letter-spacing: 0.25px;
+        text-decoration: none;
+        transition: 150ms all ease-in-out;
+    }
+    li:hover > a {
+        color: var(--color-font);
+    }
+</style>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Menu from "./Menu.svelte";
+</script>
+
+<div class="sidebar">
+    <img src="/images/logo.svg" class="logo">
+    <Menu />
+</div>
+
+<style>
+    .sidebar {
+        border-right: 1px solid var(--color-border);
+        height: 100%;
+        width: calc(var(--spacing) * 72);
+    }
+    .logo {
+        border: 1px solid red;
+        /* margin: 0 auto; */
+        margin-inline: 0 auto;
+        padding: 1rem;
+        width: calc(var(--spacing) * 48);
+    }
+</style>
